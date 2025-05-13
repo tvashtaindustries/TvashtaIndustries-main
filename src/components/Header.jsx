3a2d7162aed7679f0c1ee5ab6dbf43d99ac63ab3
Vivek Banduni <Vivekbanduni3@gmail.com>
2025-05-13 12:10:01 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
-
+import React from 'react';
 import './Header.css';
-import SearchBar from './SearchBar'
 
 const Header = () => {
   return (
     <header className="header">
-      <h1 className='header-title'>Tvashta Industries</h1>
-      <SearchBar></SearchBar>
+      <h1 className="logo">Tvastha Industries</h1>
     </header>
   );
 };
